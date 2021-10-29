3dcc751fdccd777cd4a00ecba1ee18ea4b1548b9
Kevin Huang <kevinhuang960@gmail.com>
2021-10-28 21:25:27 -0400

    MANY THINGS AND REFINED MOVEMENT

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -4,17 +4,60 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-	public float speed = 20f;
-	public float horizontalMove;
-    // Start is called before the first frame update
+	private Rigidbody2D rb;
+	public float speed;
+	public float jumpForce;
+	public GameObject PlayerPrefab;
+	private SpriteRenderer sr;
+	private bool grounded;
+	public Transform feet;
+	public float checkRadius;
+	public LayerMask Groundwhere;
+
+	//public float horizontalMove;
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-    	horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
-    }
+    	/*float xMovement = Input.GetAxis("Horizontal");
+    	 (xMovement > 0){
+    		sr.flipX = false;
+    	}else if (xMovement < 0){
+    		sr.flipX = true;
+    	}*/
+    	if(Input.GetKey(KeyCode.D)){
+    		transform.Translate(speed*Time.deltaTime, 0, 0);
+    		sr.flipX = false;
+    	}
+    	if(Input.GetKey(KeyCode.A)){
+    		transform.Translate(-speed*Time.deltaTime, 0, 0);
+    		sr.flipX = true;
+    	}
+    	/*if(Input.GetKeyDown(KeyCode.W)){
+    		transform.Translate(0,Time.deltaTime*50,0);
+    	}*/
+    	if(Input.GetKeyDown(KeyCode.Z)){
+			SpawnEntity();
+    	}
+    	grounded = Physics2D.OverlapCircle(feet.position, checkRadius, Groundwhere);
+    	if (grounded == true && Input.GetKeyDown(KeyCode.W)){
+    		rb.velocity = Vector2.up * jumpForce;
+    	}
+
+    	//horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+    }//Quaternion Mathematics    	
+    	/*public void UseItem(){
+    		Debug.Log("MOVING")
+    	}*/
+        	public void SpawnEntity(){
+    		GameObject Player = Instantiate(PlayerPrefab, Vector3.zero, Quaternion.identity);
+			Player.GetComponent<Playermovement>().enabled = false;
+    	}
 }
+
